0c523e84ca4807647357e3cf03a98c7af4d143ef
Ivan Efimov <58842910+Laborant1@users.noreply.github.com>
2022-05-08 21:19:56 +0500

    Выполнил задание №1 ЛР 3

---
+++ Lab3/getdata_3.php
@@ -0,0 +1,16 @@
+<?php
+    $kol = $_POST['kol'];
+    $tek = $_POST['tek'];
+    
+    $mass_news = [];
+    
+    $link = mysqli_connect('localhost', 'a0664994_ajax', 'h4suMHQl', 'a0664994_ajax') or die('Ошибка! База данных не найдена!');
+    
+    $query = 'SELECT * FROM news LIMIT '.$tek.', '.$kol;
+    mysqli_query($link, 'SET NAMES utf8mb4'); 
+    $result = mysqli_query($link, $query) or die("Ошибка! Таблица не найдена!");
+    while ($row = $result->fetch_assoc()) {
+        $mass_news[] = $row;
+    }
+    echo json_encode($mass_news, JSON_UNESCAPED_UNICODE);
+?>
